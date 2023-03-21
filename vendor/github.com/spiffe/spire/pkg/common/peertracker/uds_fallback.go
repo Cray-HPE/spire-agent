a7fec0c115881279d28c96a075b63d13202cba02
Russell Bunch <doomslayer@hpe.com>
2023-03-21 11:11:48 -0500

    reset vendoring

--- vendor/github.com/spiffe/spire/pkg/common/peertracker/uds_fallback.go
+++
@@ -1,8 +0,0 @@
-//go:build !linux && !darwin && !freebsd && !netbsd && !openbsd
-// +build !linux,!darwin,!freebsd,!netbsd,!openbsd
-
-package peertracker
-
-func getCallerInfoFromFileDescriptor(fd uintptr) (CallerInfo, error) {
-	return CallerInfo{}, ErrUnsupportedPlatform
-}
